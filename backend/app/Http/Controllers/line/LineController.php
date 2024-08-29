322547817461392623351fef8cf5896d6150f470
phuwadech <aofphuwadech@gmail.com>
2024-08-29 11:31:31 +0700

    webhook

--- backend/app/Http/Controllers/line/LineController.php
+++ backend/app/Http/Controllers/line/LineController.php
@@ -10,16 +10,15 @@ use Illuminate\Support\Facades\Log;
 
 class LineController extends Controller
 {
-    public function webhook(Request $request) : JsonResponse{
+    public function webhook(Request $request): JsonResponse
+    {
         $res = $request->all();
         $events = $res["events"];
         if (count($events) > 0) {
-//            if ($events[0]["type"] === "event") {
-//                $chatHistory = new chatHistory();
-//                $chatHistory->custId = $events[0]['source']['userId'];
-//                $chatHistory->textMessage = $events[0]['message']['text'];
-//                $chatHistory->save();
-//            }
+            $chatHistory = new chatHistory();
+            $chatHistory->custId = $events[0]['source']['userId'];
+            $chatHistory->textMessage = $events[0]['message']['text'];
+            $chatHistory->save();
         }
         Log::info('Showing request', ['request' => json_encode($res, JSON_PRETTY_PRINT)]);
         return response()->json([
