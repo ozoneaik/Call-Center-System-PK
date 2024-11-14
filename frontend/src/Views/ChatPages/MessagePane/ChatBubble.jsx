05fb8f213ce4b34fddbfc459c2d0c9e40d2d3813
phuwadech <aofphuwadech@gmail.com>
2024-11-14 22:49:01 +0700

    add audio type

--- frontend/src/Views/ChatPages/MessagePane/ChatBubble.jsx
+++ frontend/src/Views/ChatPages/MessagePane/ChatBubble.jsx
@@ -1,38 +1,38 @@
 import Stack from "@mui/joy/Stack";
 import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
-import {Sheet} from "@mui/joy";
-import {MessageStyle} from "../../../styles/MessageStyle.js";
-import {Link} from "react-router-dom";
-import {useAuth} from "../../../context/AuthContext.jsx";
+import { Sheet } from "@mui/joy";
+import { MessageStyle } from "../../../styles/MessageStyle.js";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../context/AuthContext.jsx";
 
 export default function Bubble(props) {
-    const {user} = useAuth();
-    const {sender, variant, content, created_at, contentType} = props;
+    const { user } = useAuth();
+    const { sender, variant, content, created_at, contentType } = props;
     const isSent = variant === 'sent';
     return (
-        <Box sx={{maxWidth: '60%', minWidth: 'auto'}}>
+        <Box sx={{ maxWidth: '60%', minWidth: 'auto' }}>
             <Stack direction="row" spacing={2} sx={MessageStyle.Bubble.Main}>
                 <Typography level="body-xs">
                     {isSent ? sender.name : sender.custName ? sender.custName : sender.name}
                 </Typography>
                 <Typography level="body-xs">{new Date(created_at).toLocaleString()}</Typography>
             </Stack>
-            <Box sx={{position: 'relative'}}>
+            <Box sx={{ position: 'relative' }}>
                 <Sheet sx={
                     isSent ? sender.empCode === user.empCode ? MessageStyle.Bubble.IsMySent : MessageStyle.Bubble.IsSent : MessageStyle.Bubble.IsNotSent}>
                     {
                         contentType === 'sticker' ? (
                             <Sheet variant="outlined" sx={isSent ? MessageStyle.Bubble.ImageIsSent : MessageStyle.Bubble.ImageIsNotSent}>
-                                    <img src={content} alt="" width={165}/>
-                                </Sheet>
+                                <img src={content} alt="" width={165} />
+                            </Sheet>
                         ) : contentType === 'image' ? (
                             <Sheet
                                 variant="outlined"
                                 sx={isSent ? MessageStyle.Bubble.ImageIsSent : MessageStyle.Bubble.ImageIsNotSent}>
-                                <Stack direction="row" spacing={1.5} sx={{alignItems: 'center'}}>
+                                <Stack direction="row" spacing={1.5} sx={{ alignItems: 'center' }}>
                                     <Link to={content} target={'_blank'}>
-                                        <img src={content} width={165} alt=""/>
+                                        <img src={content} width={165} alt="" />
                                     </Link>
                                 </Stack>
                             </Sheet>
@@ -40,13 +40,31 @@ export default function Bubble(props) {
                             <Sheet
                                 variant="outlined"
                                 sx={isSent ? MessageStyle.Bubble.ImageIsSent : MessageStyle.Bubble.ImageIsNotSent}>
-                                <Stack direction="row" spacing={1.5} sx={{alignItems: 'center'}}>
+                                <Stack direction="row" spacing={1.5} sx={{ alignItems: 'center' }}>
                                     <Link to={content} target={'_blank'}>
                                         <video width={200} controls preload="metadata"
                                         >
-                                            <source src={content} type="video/mp4"/>
+                                            <source src={content} type="video/mp4" />
                                             Your browser does not support the video tag.
                                         </video>
+                                    </Link>
+                                </Stack>
+                            </Sheet>
+                        ) : contentType === 'audio' ? (
+                            <Sheet
+                                variant="outlined"
+                                sx={isSent ? MessageStyle.Bubble.ImageIsSent : MessageStyle.Bubble.ImageIsNotSent}>
+                                <Stack direction="row" spacing={1.5} sx={{ alignItems: 'center' }}>
+                                    <Link to={content} target={'_blank'}>
+                                        <audio controls preload="metadata" style={{ width: 200 }}>
+                                            <source src={content} type="audio/x-m4a" />
+                                            Your browser does not support the audio tag.
+                                        </audio>
+                                        {/* <video width={200} controls preload="metadata"
+                                        >
+                                            <source src={content} type="video/mp4" />
+                                            Your browser does not support the video tag.
+                                        </video> */}
                                     </Link>
                                 </Stack>
                             </Sheet>
