776c016ceb46bc48f169daf5449aa15ed6a60ab2
ozoneaik <aofphuwadech@gmail.com>
2025-08-15 14:24:24 +0700

    update table tag group

--- backend/app/Models/TagMenu.php
+++ backend/app/Models/TagMenu.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TagMenu extends Model
 {
-    use HasFactory;
-    protected $fillable = ['tagName','group_id'];
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'tag_menus';
+
+    protected $fillable = [
+        'tagName',
+        'group_id',
+        'require_note',
+        'created_by_user_id',
+        'updated_by_user_id',
+        'deleted_by_user_id',
+    ];
+
+    protected $casts = [
+        'require_note' => 'boolean',
+    ];
 }
