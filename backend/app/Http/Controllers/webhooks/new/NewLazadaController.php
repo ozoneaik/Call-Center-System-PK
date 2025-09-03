b50f86308e3d466f120c9a84595eb326d4d695ad
phuwadech <aofphuwadech@gmail.com>
2025-09-03 11:12:36 +0700

    fix 3

--- backend/app/Http/Controllers/webhooks/new/NewLazadaController.php
+++ backend/app/Http/Controllers/webhooks/new/NewLazadaController.php
@@ -413,7 +413,7 @@ class NewLazadaController extends Controller
             }
             foreach ($sent_messages as $key => $message) {
                 $store_chat = new ChatHistory();
-                $store_chat->custId = (string) $customer['custId'];
+                $store_chat->custId = $filter_case_response['customer']['custId'];
                 $store_chat->content = $message['content'];
                 $store_chat->contentType = $message['contentType'];
                 if ($filter_case_response['type_send'] === 'normal' || $filter_case_response['type_send'] === 'present') {
