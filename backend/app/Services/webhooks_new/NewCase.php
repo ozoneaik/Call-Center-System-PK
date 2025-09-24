9fcb8014ae2adb0a56b9b32903237e2be80c56f1
parinya742 <parinyanamkaew48@gmail.com>
2025-09-24 14:54:10 +0700

    fix New Case to Default

--- backend/app/Services/webhooks_new/NewCase.php
+++ backend/app/Services/webhooks_new/NewCase.php
@@ -23,180 +23,61 @@ class NewCase
         $this->pusherService = $pusherService;
     }
 
-    // public function case($message, $customer, $platformAccessToken, $bot)
-    // {
-    //     try {
-    //         Log::channel('webhook_main')->info('ปัจจุบันเป็นเคสใหม่ ไม่เคยสร้างเคส');
-    //         $now = Carbon::now();
-
-    //         // เช็คข้อความลูกค้าว่าตรงตาม keyword หรือไม่ ถ้าตรงให้่ส่งไปยังห้องนั้นๆ
-    //         $keyword = $this->checkKeyword->check($message);
-    //         if ($keyword['status']) {
-    //             $new_rate = Rates::query()->create([
-    //                 'custId' => $customer['custId'],
-    //                 'latestRoomId' => $keyword['redirectTo'],
-    //                 'status' => 'pending',
-    //                 'rate' => 0,
-    //             ]);
-    //         } else {
-    //             $new_rate = Rates::query()->create([
-    //                 'custId' => $customer['custId'],
-    //                 'latestRoomId' => 'ROOM00',
-    //                 'status' => 'progress',
-    //                 'rate' => 0,
-    //             ]);
-    //         }
-
-    //         $new_ac = ActiveConversations::query()->create([
-    //             'custId' => $customer['custId'],
-    //             'roomId' => $new_rate['latestRoomId'],
-    //             'receiveAt' => $new_rate['status'] === 'pending' ? null : $now,
-    //             'startTime' => $new_rate['status'] === 'pending' ? null : $now,
-    //             'empCode' => $bot['empCode'],
-    //             'rateRef' => $new_rate['id']
-    //         ]);
-    //         $store_chat = ChatHistory::query()->create([
-    //             'custId' => $customer['custId'],
-    //             'content' => $message['content'],
-    //             'contentType' => $message['contentType'],
-    //             'sender' => json_encode($customer),
-    //             'conversationRef' => $new_ac['id'],
-    //             'line_message_id' => $message['line_quote_token'] ?? null,
-    //             'line_quote_token' => $message['line_quote_token'] ?? null,
-    //             'line_quoted_message_id' => $message['line_quoted_message_id'] ?? null,
-    //         ]);
-    //         $this->pusherService->sendNotification($customer['custId']);
-
-    //         $msg_bot = [];
-
-
-    //         // เช็คว่า อยู่ห้องไหน ถ้าอยู่ห้องบอท ให้ ส่งเมนูไป ถ้าไม่ใช่ ให้ส่งข้อความ ระบบได้ส่งให้เจ้าหน้าที่กรุณารอสักครู่
-    //         if ($new_rate['latestRoomId'] === 'ROOM00') {
-    //             $content = "สวัสดีคุณ" . $customer['custName'];
-    //             $content = $content . "เพื่อให้การบริการของเราดำเนินไปอย่างรวดเร็วและสะดวกยิ่งขึ้น";
-    //             $content = $content . "กรุณาเลือกหัวข้อด้านล่าง เพื่อให้เจ้าหน้าที่สามารถให้ข้อมูลและบริการท่านได้อย่างถูกต้องและรวดเร็ว ขอบคุณค่ะ/ครับ";
-    //             // $content = $content . "เนื่องจาก ในวันที่ 30/08/2568 - 01/09/2568 ทางบริษัทมีการจัดสัมนาประจำปี";
-    //             // $content = $content . "จึงทำให้การให้ข้อมูลคุณลูกค้าอาจจะล่าช้ากว่าปกติ จึงขออภัยมา ณ ที่นี่ด้วย";
-    //             // $content = $content . "เพื่อให้เจ้าหน้าที่สามารถให้ข้อมูลท่านได้ถูกต้อง กรุณาเลือก เมนู ที่ท่านต้องการติดต่อ";
-    //             return [
-    //                 'status' => true,
-    //                 'send_to_cust' => true,
-    //                 'type_send' => 'menu',
-    //                 'type_message' => 'reply',
-    //                 'messages' => [
-    //                     [
-    //                         'content' => $content,
-    //                         'contentType' => 'text'
-    //                     ]
-    //                 ],
-    //                 'customer' => $customer,
-    //                 'ac_id' => $new_ac['id'],
-    //                 'platform_access_token' => $platformAccessToken,
-    //                 'reply_token' => $message['reply_token'],
-    //                 'bot' => $bot
-    //             ];
-    //         } else {
-    //             return [
-    //                 'status' => true,
-    //                 'send_to_cust' => true,
-    //                 'type_send' => 'sended',
-    //                 'type_message' => 'reply',
-    //                 'messages' => [
-    //                     [
-    //                         'content' => 'ระบบกำลังส่งต่อให้เจ้าหน้าที่ กรุณารอซักครู่',
-    //                         'contentType' => 'text'
-    //                     ]
-    //                 ],
-    //                 'customer' => $customer,
-    //                 'ac_id' => $new_ac['id'],
-    //                 'platform_access_token' => $platformAccessToken,
-    //                 'reply_token' => $message['reply_token'],
-    //                 'bot' => $bot
-    //             ];
-    //         }
-    //         Log::channel('webhook_main')->info('สร้างเคสใหม่สำเร็จ');
-    //     } catch (\Exception $e) {
-    //         Log::channel('webhook_main')->error('เกิดข้อผิดพลาดในการสร้างเคสใหม่: ' . $e->getMessage());
-    //         return ['status' => false, 'message' => 'เกิดข้อผิดพลาดในการสร้างเคสใหม่: ' . $e->getMessage()];
-    //     }
-    // }
-
-    //กรองเคสสำหรับเคสที่คาดว่าเป็นสแปม 
     public function case($message, $customer, $platformAccessToken, $bot)
     {
         try {
             Log::channel('webhook_main')->info('ปัจจุบันเป็นเคสใหม่ ไม่เคยสร้างเคส');
             $now = Carbon::now();
 
-            $prediction = null;
-            try {
-                if ($message['contentType'] === 'image' && isset($message['file_path'])) {
-                    $response = Http::attach(
-                        'file',
-                        file_get_contents($message['file_path']),
-                        basename($message['file_path'])
-                    )->post("https://e30e4a913322.ngrok-free.app/predict");
-                } else {
-                    $response = Http::post("https://e30e4a913322.ngrok-free.app/predict_url", [
-                        'url' => $message['content']
-                    ]);
-                }
-
-                if ($response->successful()) {
-                    $prediction = $response->json('prediction');
-                }
-            } catch (\Exception $e) {
-                Log::channel('webhook_main')->error("Spam check API error: " . $e->getMessage());
-            }
-
-            if (in_array($prediction, ['GREETING', 'NSFW'])) {
-                $roomId = 'ROOM12';   // ห้องสแปม
-                $status = 'pending';
+            // เช็คข้อความลูกค้าว่าตรงตาม keyword หรือไม่ ถ้าตรงให้่ส่งไปยังห้องนั้นๆ
+            $keyword = $this->checkKeyword->check($message);
+            if ($keyword['status']) {
+                $new_rate = Rates::query()->create([
+                    'custId' => $customer['custId'],
+                    'latestRoomId' => $keyword['redirectTo'],
+                    'status' => 'pending',
+                    'rate' => 0,
+                ]);
             } else {
-                $keyword = $this->checkKeyword->check($message);
-                if ($keyword['status']) {
-                    $roomId = $keyword['redirectTo'];
-                    $status = 'pending';
-                } else {
-                    $roomId = 'ROOM00'; // ห้องบอท
-                    $status = 'progress';
-                }
+                $new_rate = Rates::query()->create([
+                    'custId' => $customer['custId'],
+                    'latestRoomId' => 'ROOM00',
+                    'status' => 'progress',
+                    'rate' => 0,
+                ]);
             }
-
-            $new_rate = Rates::query()->create([
-                'custId' => $customer['custId'],
-                'latestRoomId' => $roomId,
-                'status' => $status,
-                'rate' => 0,
-            ]);
 
             $new_ac = ActiveConversations::query()->create([
                 'custId' => $customer['custId'],
-                'roomId' => $roomId,
-                'receiveAt' => $status === 'pending' ? null : $now,
-                'startTime' => $status === 'pending' ? null : $now,
+                'roomId' => $new_rate['latestRoomId'],
+                'receiveAt' => $new_rate['status'] === 'pending' ? null : $now,
+                'startTime' => $new_rate['status'] === 'pending' ? null : $now,
                 'empCode' => $bot['empCode'],
                 'rateRef' => $new_rate['id']
             ]);
-
-            ChatHistory::query()->create([
+            $store_chat = ChatHistory::query()->create([
                 'custId' => $customer['custId'],
                 'content' => $message['content'],
                 'contentType' => $message['contentType'],
                 'sender' => json_encode($customer),
                 'conversationRef' => $new_ac['id'],
-                'line_message_id' => $message['line_message_id'] ?? null,
+                'line_message_id' => $message['line_quote_token'] ?? null,
                 'line_quote_token' => $message['line_quote_token'] ?? null,
                 'line_quoted_message_id' => $message['line_quoted_message_id'] ?? null,
             ]);
-
             $this->pusherService->sendNotification($customer['custId']);
 
-            if ($roomId === 'ROOM00') {
-                $content = "สวัสดีคุณ" . $customer['custName'];
-                $content .= " เพื่อให้การบริการของเราดำเนินไปอย่างรวดเร็วและสะดวกยิ่งขึ้น";
-                $content .= " กรุณาเลือกหัวข้อด้านล่าง เพื่อให้เจ้าหน้าที่สามารถให้ข้อมูลและบริการท่านได้อย่างถูกต้องและรวดเร็ว ขอบคุณค่ะ/ครับ";
+            $msg_bot = [];
 
+
+            // เช็คว่า อยู่ห้องไหน ถ้าอยู่ห้องบอท ให้ ส่งเมนูไป ถ้าไม่ใช่ ให้ส่งข้อความ ระบบได้ส่งให้เจ้าหน้าที่กรุณารอสักครู่
+            if ($new_rate['latestRoomId'] === 'ROOM00') {
+                $content = "สวัสดีคุณ" . $customer['custName'];
+                $content = $content . "เพื่อให้การบริการของเราดำเนินไปอย่างรวดเร็วและสะดวกยิ่งขึ้น";
+                $content = $content . "กรุณาเลือกหัวข้อด้านล่าง เพื่อให้เจ้าหน้าที่สามารถให้ข้อมูลและบริการท่านได้อย่างถูกต้องและรวดเร็ว ขอบคุณค่ะ/ครับ";
+                // $content = $content . "เนื่องจาก ในวันที่ 30/08/2568 - 01/09/2568 ทางบริษัทมีการจัดสัมนาประจำปี";
+                // $content = $content . "จึงทำให้การให้ข้อมูลคุณลูกค้าอาจจะล่าช้ากว่าปกติ จึงขออภัยมา ณ ที่นี่ด้วย";
+                // $content = $content . "เพื่อให้เจ้าหน้าที่สามารถให้ข้อมูลท่านได้ถูกต้อง กรุณาเลือก เมนู ที่ท่านต้องการติดต่อ";
                 return [
                     'status' => true,
                     'send_to_cust' => true,
@@ -208,19 +89,6 @@ class NewCase
                             'contentType' => 'text'
                         ]
                     ],
-                    'customer' => $customer,
-                    'ac_id' => $new_ac['id'],
-                    'platform_access_token' => $platformAccessToken,
-                    'reply_token' => $message['reply_token'],
-                    'bot' => $bot
-                ];
-            } elseif ($roomId === 'ROOM12') {
-                return [
-                    'status' => true,
-                    'send_to_cust' => false,
-                    'type_send' => 'spam',
-                    'type_message' => 'system',
-                    'messages' => [],
                     'customer' => $customer,
                     'ac_id' => $new_ac['id'],
                     'platform_access_token' => $platformAccessToken,
@@ -246,11 +114,143 @@ class NewCase
                     'bot' => $bot
                 ];
             }
+            Log::channel('webhook_main')->info('สร้างเคสใหม่สำเร็จ');
         } catch (\Exception $e) {
             Log::channel('webhook_main')->error('เกิดข้อผิดพลาดในการสร้างเคสใหม่: ' . $e->getMessage());
-            return ['status' => false, 'message' => 'เกิดข้อผิดพลาด: ' . $e->getMessage()];
+            return ['status' => false, 'message' => 'เกิดข้อผิดพลาดในการสร้างเคสใหม่: ' . $e->getMessage()];
         }
     }
+
+    //กรองเคสสำหรับเคสที่คาดว่าเป็นสแปม 
+    // public function case($message, $customer, $platformAccessToken, $bot)
+    // {
+    //     try {
+    //         Log::channel('webhook_main')->info('ปัจจุบันเป็นเคสใหม่ ไม่เคยสร้างเคส');
+    //         $now = Carbon::now();
+
+    //         $prediction = null;
+    //         try {
+    //             if ($message['contentType'] === 'image' && isset($message['file_path'])) {
+    //                 $response = Http::attach(
+    //                     'file',
+    //                     file_get_contents($message['file_path']),
+    //                     basename($message['file_path'])
+    //                 )->post("https://e30e4a913322.ngrok-free.app/predict");
+    //             } else {
+    //                 $response = Http::post("https://e30e4a913322.ngrok-free.app/predict_url", [
+    //                     'url' => $message['content']
+    //                 ]);
+    //             }
+
+    //             if ($response->successful()) {
+    //                 $prediction = $response->json('prediction');
+    //             }
+    //         } catch (\Exception $e) {
+    //             Log::channel('webhook_main')->error("Spam check API error: " . $e->getMessage());
+    //         }
+
+    //         if (in_array($prediction, ['GREETING', 'NSFW'])) {
+    //             $roomId = 'ROOM12';   // ห้องสแปม
+    //             $status = 'pending';
+    //         } else {
+    //             $keyword = $this->checkKeyword->check($message);
+    //             if ($keyword['status']) {
+    //                 $roomId = $keyword['redirectTo'];
+    //                 $status = 'pending';
+    //             } else {
+    //                 $roomId = 'ROOM00'; // ห้องบอท
+    //                 $status = 'progress';
+    //             }
+    //         }
+
+    //         $new_rate = Rates::query()->create([
+    //             'custId' => $customer['custId'],
+    //             'latestRoomId' => $roomId,
+    //             'status' => $status,
+    //             'rate' => 0,
+    //         ]);
+
+    //         $new_ac = ActiveConversations::query()->create([
+    //             'custId' => $customer['custId'],
+    //             'roomId' => $roomId,
+    //             'receiveAt' => $status === 'pending' ? null : $now,
+    //             'startTime' => $status === 'pending' ? null : $now,
+    //             'empCode' => $bot['empCode'],
+    //             'rateRef' => $new_rate['id']
+    //         ]);
+
+    //         ChatHistory::query()->create([
+    //             'custId' => $customer['custId'],
+    //             'content' => $message['content'],
+    //             'contentType' => $message['contentType'],
+    //             'sender' => json_encode($customer),
+    //             'conversationRef' => $new_ac['id'],
+    //             'line_message_id' => $message['line_message_id'] ?? null,
+    //             'line_quote_token' => $message['line_quote_token'] ?? null,
+    //             'line_quoted_message_id' => $message['line_quoted_message_id'] ?? null,
+    //         ]);
+
+    //         $this->pusherService->sendNotification($customer['custId']);
+
+    //         if ($roomId === 'ROOM00') {
+    //             $content = "สวัสดีคุณ" . $customer['custName'];
+    //             $content .= " เพื่อให้การบริการของเราดำเนินไปอย่างรวดเร็วและสะดวกยิ่งขึ้น";
+    //             $content .= " กรุณาเลือกหัวข้อด้านล่าง เพื่อให้เจ้าหน้าที่สามารถให้ข้อมูลและบริการท่านได้อย่างถูกต้องและรวดเร็ว ขอบคุณค่ะ/ครับ";
+
+    //             return [
+    //                 'status' => true,
+    //                 'send_to_cust' => true,
+    //                 'type_send' => 'menu',
+    //                 'type_message' => 'reply',
+    //                 'messages' => [
+    //                     [
+    //                         'content' => $content,
+    //                         'contentType' => 'text'
+    //                     ]
+    //                 ],
+    //                 'customer' => $customer,
+    //                 'ac_id' => $new_ac['id'],
+    //                 'platform_access_token' => $platformAccessToken,
+    //                 'reply_token' => $message['reply_token'],
+    //                 'bot' => $bot
+    //             ];
+    //         } elseif ($roomId === 'ROOM12') {
+    //             return [
+    //                 'status' => true,
+    //                 'send_to_cust' => false,
+    //                 'type_send' => 'spam',
+    //                 'type_message' => 'system',
+    //                 'messages' => [],
+    //                 'customer' => $customer,
+    //                 'ac_id' => $new_ac['id'],
+    //                 'platform_access_token' => $platformAccessToken,
+    //                 'reply_token' => $message['reply_token'],
+    //                 'bot' => $bot
+    //             ];
+    //         } else {
+    //             return [
+    //                 'status' => true,
+    //                 'send_to_cust' => true,
+    //                 'type_send' => 'sended',
+    //                 'type_message' => 'reply',
+    //                 'messages' => [
+    //                     [
+    //                         'content' => 'ระบบกำลังส่งต่อให้เจ้าหน้าที่ กรุณารอซักครู่',
+    //                         'contentType' => 'text'
+    //                     ]
+    //                 ],
+    //                 'customer' => $customer,
+    //                 'ac_id' => $new_ac['id'],
+    //                 'platform_access_token' => $platformAccessToken,
+    //                 'reply_token' => $message['reply_token'],
+    //                 'bot' => $bot
+    //             ];
+    //         }
+    //     } catch (\Exception $e) {
+    //         Log::channel('webhook_main')->error('เกิดข้อผิดพลาดในการสร้างเคสใหม่: ' . $e->getMessage());
+    //         return ['status' => false, 'message' => 'เกิดข้อผิดพลาด: ' . $e->getMessage()];
+    //     }
+    // }
 
     public static function formatBotMenu($custName, $platForm, $platFrom_id)
     {
